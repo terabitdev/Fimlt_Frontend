64b1fcf2ce7ae9ae007908a359898919aae61f16
JunaidAly <juanidalikhan03@gmail.com>
2025-05-16 17:14:27 +0500

    updated

--- src/Components/UserManagement/ProjectDetails.jsx
+++ src/Components/UserManagement/ProjectDetails.jsx
@@ -231,6 +231,7 @@ function ProjectDetails() {
             );
           } catch (e) {
             console.warn('Could not load model URL:', e);
+            console.error("Error fetching model URL:", e);
           }
         }
         
